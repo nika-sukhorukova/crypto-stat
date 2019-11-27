f1b3b14a6f41271b3007d0beac8c0923f9f7f5d8
Nika <44133262+nika-sukhorukova@users.noreply.github.com>
2019-11-27 22:06:38 +0300

    fix 8

--- src/components/dashboard/constants.js
+++ src/components/dashboard/constants.js
@@ -11,5 +11,5 @@ export const BASE_PATH = 'https://api.coinranking.com/v1/public/coins';
 export const SORT_PATH = '?sort=';
 export const TIME_PERIOD_PATH = '&timePeriod=';
 export const CURENCY_PATH = '?base=';
-export const LIMIT_PATH = '?limit=';
+export const LIMIT_PATH = '&limit=';
 
