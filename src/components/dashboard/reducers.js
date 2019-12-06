9bd8548b8c4d6c1ba68eef432483bfb262cdf83e
Nika <44133262+nika-sukhorukova@users.noreply.github.com>
2019-12-06 22:13:12 +0300

    fix17

--- src/components/dashboard/reducers.js
+++ src/components/dashboard/reducers.js
@@ -1,35 +1,35 @@
 import * as c from './constants';
 
 const initialState = {
-    isLoading: true,
-    timePeriod: '24h',
-    limit: 50,
-    offset: 0,
-    baseSymbol: 'USD',
-    sort: 'coinranking',
-    order: 'desc',
-    activeColumn:'coinranking',
-    base: {},
-    coins: [],
-    stats: {}
+	isLoading: true,
+	timePeriod: '24h',
+	limit: 50,
+	offset: 0,
+	baseSymbol: 'USD',
+	sort: 'coinranking',
+	order: 'desc',
+	activeColumn: 'coinranking',
+	base: {},
+	coins: [],
+	stats: {}
 };
 
 const DashReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case c.INIT:
-            return {
-                ...state,
-                ...action.data,
-                isLoading: false
-            };
-        case c.LOAD_DATA:
-            return {
-                ...state,
-                ...action.data
-            };
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case c.INIT:
+			return {
+				...state,
+				...action.data,
+				isLoading: false
+			};
+		case c.LOAD_DATA:
+			return {
+				...state,
+				...action.data
+			};
+		default:
+			return state;
+	}
 };
 
 export default DashReducer;
